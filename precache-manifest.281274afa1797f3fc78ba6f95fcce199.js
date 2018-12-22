3245e20eac57a5d5acc3b15a6d569b022c8d4093
CoderPrans <pranavbhaskar17@gmail.com>
2018-12-22 20:56:43 +0530

    Updates

---
+++ precache-manifest.281274afa1797f3fc78ba6f95fcce199.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d01edacba7eb1e590af6",
+    "url": "/Dungeon-Crawler/static/js/runtime~main.d01edacb.js"
+  },
+  {
+    "revision": "76ce3d667196b1164659",
+    "url": "/Dungeon-Crawler/static/js/main.76ce3d66.chunk.js"
+  },
+  {
+    "revision": "f715b2e0bea123466b14",
+    "url": "/Dungeon-Crawler/static/js/1.f715b2e0.chunk.js"
+  },
+  {
+    "revision": "76ce3d667196b1164659",
+    "url": "/Dungeon-Crawler/static/css/main.3704cb66.chunk.css"
+  },
+  {
+    "revision": "0e9dc8525ba109ec35e4ef339236cb29",
+    "url": "/Dungeon-Crawler/index.html"
+  }
+];
