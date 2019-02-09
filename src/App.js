213976296e0389a37af7538b7c54671b3a5a46b5
CoderPrans <pranavbhaskar17@gmail.com>
2019-02-09 12:33:13 +0530

    initializing handleCombat()

--- src/App.js
+++ src/App.js
@@ -2,20 +2,20 @@ import React from "react";
 import "./App.css";
 import map from "./Maps";
 
-/*const enemy = [
+const enemy = [
   { hp: 50, attack: 15 }, // color: purple, attack: 10
   { hp: 75, attack: 45 }, // color: brown, attack: 35
   { hp: 100, attack: 60 }, // color: blue, attack: 50
   { hp: 150, attack: 80 } // color: black, attack: 80
 ];
-*/
 
-/* const weapon = {
-  hand: 15,
-  hammer: 45,
-  sword: 60,
-  fire: 80
-}; */
+
+const weapon = [
+  ["hand",  15],
+  ["hammer", 45],
+  ["sword", 60],
+  ["fire", 80]
+]; 
 
 let positions = new Array(60);
 
@@ -42,8 +42,6 @@ class App extends React.Component {
       enemyDir: null,
       weaponLevel: 1,
       enemyLevel: null,
-      enemyHp: null,
-      enemyAttack: null,
       fightOn: false
     };
     this.handleTravel = this.handleTravel.bind(this);
@@ -69,37 +67,26 @@ class App extends React.Component {
  detectEnemy(nextY, nextX){
     
    if(nextY && nextX && Math.abs(nextX - 1) && Math.abs(nextY - 1) && nextY + 1 < 59 && nextX + 1 < 29){
-      // let enemyDir;
-      // let enemyLevel;
     if(positions[nextY + 1][nextX] === 1){
       alert('Enemy detected!, right');
       console.log("enemy detected", positions[nextY + 1][nextX])
       this.setState({fightOn: true})
       return {enemyDir: "right", enemyLevel: 1}
-       //enemyDir = "right"
-      //enemyLevel = positions[this.state.posY + 1][this.state.posX]
       } else if(positions[nextY][nextX + 1] === 1){
       alert('Enemy detected!, down');
       console.log("enemy detected", "down")
       this.setState({fightOn: true})
       return {enmeyDir: "down", enemyLevel: 1}
-      //enemyDir = "down"
-      //enemyLevel = positions[this.state.posY][this.state.posX + 1]
-     
     } else if(positions[nextY - 1][nextX] === 1){
       alert('Enemy detected!, left');
       console.log("enemy detected", "left")
       this.setState({fightOn: true})
       return {enemyDir: "left", enemyLevel: 1}
-      //enemyDir = "left"
-      //enemyLevel = positions[this.state.posY -1][this.state.posX] this.setState({fightOn: true})
     } else if(positions[nextY][nextX - 1] === 1){
       alert('Enemy detected!, up');
       console.log("enemy detected", "up")
       this.setState({fightOn: true})
       return {enemyDir: "up", enemyLevel: 1}
-     //enemyDir = "up"
-      //enemyLevel = positions[this.state.posY][this.state.posX - 1]
     }
     
    console.log("from inside the detectEnemy", this.state.fightOn)
@@ -107,16 +94,6 @@ class App extends React.Component {
     }
   
   }
-
-  /*componentDidUpdate(prevProps, prevState){
-    console.log(this.state.posX, prevState.posX);
-    if(this.state.posX !== prevState.posX && this.state.posY !== prevState.posY){
-      this.detectEnemy()
-      console.log("inside state check");
-    }
-    console.log(this.state.fightOn ? "fight!!!!!!!!!!!!" : "dont fight")
-  }*/
-
 
   handleTravel(e) {
     e.preventDefault();
@@ -128,7 +105,7 @@ class App extends React.Component {
         map[posY + 1].indexOf(posX) < 0 &&
         positions[posY + 1][posX] !== 1
       ) {
-        if(this.detectEnemy(posY + 1, posX) === 'object'){
+        if(typeof this.detectEnemy(posY + 1, posX) === 'object'){
           let {enemyDir, enemyLevel} = this.detectEnemy(posY + 1, posX)
           this.setState({ enemyDir, enemyLevel })
         } else {
@@ -186,13 +163,19 @@ class App extends React.Component {
        
 
   handleCombat(){
-    if(this.state.fight){
+    if(this.state.fightOn){
        // simulate fight scenario
       // detect thug level
       // detect weapon level
       // block passage unless the thug is defeated 
       // decrease the hp of user (according to thug level)  and the hp of thug (according to weapon level)
       // increase the xp of user (according to thug level)
+   /* let {hp, xp, enemyLevel, weaponLevel, enemyDir} = this.state, 
+          enemyHp = enemy[enemyLevel].hp,
+          enemyAttack = enemy[enemyLevel].attack,
+          weaponName = weapon[weaponLevel][0],
+          weaponLevel = weapon[weaponLevel][1]
+  */
     } 
   }
 
