2d1259d0f263d31b555178d147389d7eac3f3ca7
CoderPrans <pranavbhaskar17@gmail.com>
2019-03-22 00:40:52 +0530

    Updates

--- precache-manifest.fe799199dae9b9e82d597e7ec14f389c.js
+++ precache-manifest.fe799199dae9b9e82d597e7ec14f389c.js
@@ -4,8 +4,8 @@ self.__precacheManifest = [
     "url": "/Dungeon-Crawler/static/media/enemy.765b5373.png"
   },
   {
-    "revision": "cd2a5f26ea52fb1d77e4",
-    "url": "/Dungeon-Crawler/static/css/main.7ff3b523.chunk.css"
+    "revision": "f6215c8cb38c9429486d",
+    "url": "/Dungeon-Crawler/static/css/main.4df19440.chunk.css"
   },
   {
     "revision": "947ab1e0180e62f5ea86",
@@ -20,28 +20,28 @@ self.__precacheManifest = [
     "url": "/Dungeon-Crawler/static/media/hero.bd6df380.png"
   },
   {
-    "revision": "d3b3d9c57fe7def44ef6bdc88b06d6de",
-    "url": "/Dungeon-Crawler/static/media/path-tile.d3b3d9c5.png"
-  },
-  {
     "revision": "2c37d3e0d1f6aa4cdab81745db8839bb",
     "url": "/Dungeon-Crawler/static/media/brick-wall.2c37d3e0.png"
   },
   {
-    "revision": "cd2a5f26ea52fb1d77e4",
-    "url": "/Dungeon-Crawler/static/js/main.cd2a5f26.chunk.js"
+    "revision": "d3b3d9c57fe7def44ef6bdc88b06d6de",
+    "url": "/Dungeon-Crawler/static/media/path-tile.d3b3d9c5.png"
+  },
+  {
+    "revision": "f6215c8cb38c9429486d",
+    "url": "/Dungeon-Crawler/static/js/main.f6215c8c.chunk.js"
   },
   {
     "revision": "1971b74e3e7d94c0ae2a6dde578357ae",
     "url": "/Dungeon-Crawler/static/media/boss.1971b74e.png"
   },
   {
-    "revision": "abd2fddd815ded765ef7057585779201",
-    "url": "/Dungeon-Crawler/static/media/hammer.abd2fddd.png"
-  },
-  {
     "revision": "21cc460aff3ed8bdfcf10250773475a1",
     "url": "/Dungeon-Crawler/static/media/hp.21cc460a.png"
+  },
+  {
+    "revision": "abd2fddd815ded765ef7057585779201",
+    "url": "/Dungeon-Crawler/static/media/hammer.abd2fddd.png"
   },
   {
     "revision": "7f09f3494413ab1d1ab34246523d1f51",
@@ -52,7 +52,7 @@ self.__precacheManifest = [
     "url": "/Dungeon-Crawler/static/media/fire.65743c36.png"
   },
   {
-    "revision": "cf0c0679b4338cdf0c8c1bfbd54c866b",
+    "revision": "dedd133b93bc802ac54282502a0f9b3f",
     "url": "/Dungeon-Crawler/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Dungeon-Crawler/precache-manifest.b3135ad4d979e793ea19cd814505760f.js"
+  "/Dungeon-Crawler/precache-manifest.fe799199dae9b9e82d597e7ec14f389c.js"
 );
 
 workbox.clientsClaim();
