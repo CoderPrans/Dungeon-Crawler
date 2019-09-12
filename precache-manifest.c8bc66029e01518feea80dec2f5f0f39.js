369e9fb841f7a35be278941baddf3bad7c14476b
Pranav Bhaskar <pranavbhaskar17@gmail.com>
2019-09-12 22:39:35 +0530

    Updates

---
+++ precache-manifest.c8bc66029e01518feea80dec2f5f0f39.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "2317bce5191c6f12e358e0f46d501c39",
+    "url": "/Dungeon-Crawler/static/media/sword.2317bce5.png"
+  },
+  {
+    "revision": "7a0ac0d2ddc883dec3b3",
+    "url": "/Dungeon-Crawler/static/css/main.52e7a497.chunk.css"
+  },
+  {
+    "revision": "885e8112ca600fd16985",
+    "url": "/Dungeon-Crawler/static/js/1.885e8112.chunk.js"
+  },
+  {
+    "revision": "d01edacba7eb1e590af6",
+    "url": "/Dungeon-Crawler/static/js/runtime~main.d01edacb.js"
+  },
+  {
+    "revision": "687d7054a593efec947a4fb7a6a53d84",
+    "url": "/Dungeon-Crawler/static/media/footsteps.687d7054.mp3"
+  },
+  {
+    "revision": "2902508dfcc168b45a9068ccbd72b529",
+    "url": "/Dungeon-Crawler/static/media/picked.2902508d.mp3"
+  },
+  {
+    "revision": "712a1734fa23b1a3bf7e894fdb77a235",
+    "url": "/Dungeon-Crawler/static/media/hp.712a1734.png"
+  },
+  {
+    "revision": "e82ef265545ff132abae0a72c00ad51b",
+    "url": "/Dungeon-Crawler/static/media/hammer.e82ef265.png"
+  },
+  {
+    "revision": "99f6a8e21c798d715cfad06002c56cde",
+    "url": "/Dungeon-Crawler/static/media/fire.99f6a8e2.png"
+  },
+  {
+    "revision": "7a0ac0d2ddc883dec3b3",
+    "url": "/Dungeon-Crawler/static/js/main.7a0ac0d2.chunk.js"
+  },
+  {
+    "revision": "6b4a77862ddbf3dbcfcdabcff6b7ce32",
+    "url": "/Dungeon-Crawler/static/media/punch-blast.6b4a7786.png"
+  },
+  {
+    "revision": "b07bdeebafe38d8feb03765116643d76",
+    "url": "/Dungeon-Crawler/static/media/enemy.b07bdeeb.png"
+  },
+  {
+    "revision": "cb45efcaa756f55bcb8fbe046b41d00f",
+    "url": "/Dungeon-Crawler/static/media/hero.cb45efca.png"
+  },
+  {
+    "revision": "e6dfff7765ce211b0fa761bcc0aac689",
+    "url": "/Dungeon-Crawler/static/media/fight-begin.e6dfff77.mp3"
+  },
+  {
+    "revision": "4849f499177a231c53e6644eed606428",
+    "url": "/Dungeon-Crawler/static/media/dying.4849f499.mp3"
+  },
+  {
+    "revision": "947458669d01eb6035b6696743cc4ee1",
+    "url": "/Dungeon-Crawler/static/media/brick-wall.94745866.png"
+  },
+  {
+    "revision": "bbe88ce9d71462585774a658740be4d8",
+    "url": "/Dungeon-Crawler/static/media/path-tile.bbe88ce9.png"
+  },
+  {
+    "revision": "1971b74e3e7d94c0ae2a6dde578357ae",
+    "url": "/Dungeon-Crawler/static/media/boss.1971b74e.png"
+  },
+  {
+    "revision": "5147a29223ccba2fd0dfeb6d53cf091c",
+    "url": "/Dungeon-Crawler/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Dungeon-Crawler/precache-manifest.5cd573eaa229ba105c3e511e463b8eae.js"
+  "/Dungeon-Crawler/precache-manifest.c8bc66029e01518feea80dec2f5f0f39.js"
 );
 
 workbox.clientsClaim();
